20897821a89eb182a8d2bf5fd1836b0c9ad80b0d
ozelbot <44919025+ozelbot@users.noreply.github.com>
2018-11-10 11:20:25 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sa') {
+    msg.reply('AleykümSelam');
+  }
+
+  if (msg.content === 'istiklalmarşı') {
+    msg.reply('Yakında...');
+  }
+
+  if (msg.content === 'ping') {
+    msg.reply('Yapım Aşamasındadır');
+  }
+
+if (msg.content === 'ping') {
+  msg.channel.send('ping' + client.ping + 'ms');
+}
+
+if (msg.content === 'avatarım') {
+    msg.channel.send(msg.author.avatarURL);
+}
+
+});
+
+client.login('NTEwNDkzODEzOTE2MTcyMzAw.DsdhRA.LBZJc7JjhJPEGiAoGrBjPBGUeVc');
